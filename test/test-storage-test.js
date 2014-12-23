5197b56125801eeb17c32f01100a51c991b19ae9
Vasily Kuznetsov <kvas.it@gmail.com>
2014-12-23 21:10:10 +0100

    Implement storage of types in the test storage.

--- test/test-storage-test.js
+++ test/test-storage-test.js
@@ -7,8 +7,16 @@ require('should');
 
 describe('test-storage', function () {
 
-    it('should create storage', function () {
+    it('should store types', function () {
         var storage = new riki.TestStorage();
+
+        return storage.addType('test-type', {title: 'Test type'})
+        .then(function () {
+            return storage.getType('test-type');
+        })
+        .then(function (type) {
+            type.title.should.eql('Test type');
+        });
     });
 
 });
